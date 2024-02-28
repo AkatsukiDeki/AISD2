554cc10943e5e90a0efef48f9cb98a2ba4b81552
AkatsukiDeki <124888750+AkatsukiDeki@users.noreply.github.com>
2024-02-28 17:44:16 +0400

    12 var 5 easy task

--- LinkedList.h
+++ LinkedList.h
@@ -41,5 +41,44 @@ public:
 
     void remove_duplicates();
 };
+template <typename T>
+void LinkedList<T>::remove_duplicates() {
+    Node<T>* current = head;
+
+    while (current != nullptr) {
+        Node<T>* runner = current->next;
+
+        while (runner != nullptr) {
+            if (current->data == runner->data) {
+                Node<T>* duplicate = runner;
+                runner = runner->next;
+
+                if (duplicate == tail) {
+                    tail = duplicate->prev;
+                }
+
+                if (duplicate == head) {
+                    head = duplicate->next;
+                }
+
+                if (duplicate->prev) {
+                    duplicate->prev->next = duplicate->next;
+                }
+
+                if (duplicate->next) {
+                    duplicate->next->prev = duplicate->prev;
+                }
+
+                delete duplicate;
+            }
+            else {
+                runner = runner->next;
+            }
+        }
+
+        current = current->next;
+    }
+};
+
 
 #endif
